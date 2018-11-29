5177256cedec03cee5bd9658d00420c4a11a8034
起床啦！ <1106728884@qq.com>
2018-11-29 14:42:45 +0800

    功能添加

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-1
+# echarts vue版组件
+
+### 1. 适用前提--必须引入echarts
+> npm install echarts
+
+### 2. 安装依赖
+> npm i vue-charts-block
+
+### 3. 声明
+>
+	//根目录下src--main.js文件中
+	import VueChartsBlock from 'vue-charts-block'
+	//底下引用
+	Vue.use(VueChartsBlock)
+>
+
+### 4. 使用
+>
+	<vue-charts-block></vue-charts-block>
+>
+
+### 5. 参数
+> * 导航信息:*navInformation*-->Array;必传
+> * 自定义class:*cname*-->String;非必传;默认`'ele-nav'`
+> * 自定义内联样式:*myStyle*-->String;非必传;默认`''`
+> * 是否开启横向导航:*horizontal*-->Boolean;非必传;默认`false`
+> * 是否开启手风琴模式:*accordion*-->Boolean;非必传;默认`false`
+
+### 6. 样式
+> * 自定义颜色:组件上绑定cname后，复制原本的*.ele-nav*样式进行颜色，注意 在.el-menu--horizontal{}外层一定要加一层body，不然没法自定义横向样式
--- src/lib/VueChartsBlock.vue
+++ src/lib/VueChartsBlock.vue
@@ -4,20 +4,81 @@
 */
 /*
 * @LastEditors: aFei
-* @LastEditTime: 2018-11-29 10:48:39
+* @LastEditTime: 2018-11-29 14:38:01
 */
 <template>
-    <div>
-        1
-    </div>
+  <div :id="id" style="height: 800px;"></div>
 </template>
 
 <script>
-    export default {
-        name: "VueChartsBlock"
+  let echarts = require('echarts/lib/echarts');
+  require('echarts/lib/chart/bar');
+  require('echarts/lib/chart/line');
+  require("echarts/lib/chart/pie");
+  require('echarts/lib/component/tooltip');
+  require('echarts/lib/component/tooltip');
+  require('echarts/lib/component/legend');
+
+  export default {
+    name: "VueChartsBlock",
+    props: {
+      id: {
+        required: true
+      },
+      type: {
+        type: String,
+        required: true
+      },
+      optionData: {
+        type: Object,
+        required: true
+      }
+    },
+    data() {
+      return {
+        charts: ''
+      }
+    },
+    methods: {
+      change() {
+        this.charts.resize();//Echarts重绘方法
+      }
+    },
+    created() {
+    },
+    mounted() {
+      let vue = this; // 定义闭包变量
+      this.charts = echarts.init(document.getElementById(this.id));
+      this.charts.setOption({
+        legend: {
+          data: this.optionData.legend
+        },
+        xAxis: {
+          type: 'category',
+          data: this.optionData.xAxis
+        },
+        yAxis: {
+          type: 'value'
+        },
+        color: ['#fea20f', '#4cc57f', '#3da9f7', '#7ac200', '#fc595d', '#b959ff'],
+        series: this.optionData.series
+      }, true);
+      window.addEventListener('resize', vue.change);
+    },
+    beforeDestroy() {
+      let vue = this;
+      window.removeEventListener('resize', vue.change);
     }
+  }
 </script>
 
+<!--基础样式-->
 <style scoped>
+  @import "VueChartsBlock.css";
+</style>
 
+<!--样式扩展-->
+<style scoped>
+  /*必需的扩展*/
+  @import "chartExtra.css";
 </style>
