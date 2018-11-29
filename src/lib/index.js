947553d689d8be77593aea0dd1d7dcb9a15c443b
起床啦！ <1106728884@qq.com>
2018-11-29 10:13:02 +0800

    打包优化

--- src/lib/index.js
+++ src/lib/index.js
@@ -4,9 +4,9 @@
 */
 /*
 * @LastEditors: aFei
-* @LastEditTime: 2018-11-28 16:18:20
+* @LastEditTime: 2018-11-29 10:09:24
 */
-import charts from './chart';
+import charts from './charts';
 
 const comment = {
     install(Vue) {
