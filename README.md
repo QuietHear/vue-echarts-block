971607a88a02253da8afb129ab2791665d3fb653
起床啦！ <1106728884@qq.com>
2018-12-03 11:50:48 +0800

    tooltip，toolbox添加

--- README.md
+++ README.md
@@ -18,14 +18,40 @@
 >
 	<vue-charts-block :id="id" :optionData="optionData" :type="type"></vue-charts-block>
 	// 数据示例
-	legend:{ // 图例设置
+	legend:{ // 图例
 		data: [], // 数据
-		x:'left', // 对齐方式，默认'center'
+		x:'left', // 对齐方式('left','center','right')，默认'center'
 		itemWidth: 12, // 图例小方块宽，默认12
 		itemHeight: 12, // 图例小方块高，默认12
 		color: '#e4393c', // 图例文字颜色，默认'#000'
 		fontSize: 12, // 图例文字字号，默认12
-	}, 
+	},
+	tooltip:{ // 数据提示框
+		show: true, // 是否展示，默认false
+		trigger: 'axis', // 触发方式('item','axis')，默认'item'
+		formatter:'', // 提示内容模板，abcde对应值具体查看官网，默认null
+		axisPointer:{ // 提示中轴线
+			type:'line', // 中轴线格式('line','cross','shadow','none')，默认'line'
+			lineColor:'', // line的颜色，默认'#48b'
+			lineWidth:'', // line的宽度，默认2
+			lineStyle:'', // line的形态('solid','dashed','double','auto')，默认'solid'
+			crossColor:'', // cross的颜色，默认'#1e90ff'
+			crossWidth:'', // cross的宽度，默认1
+			crossStyle:'', // cross的形态('solid','dashed','double','auto')，默认'dashed'
+			shadowColor:'', // shadow的颜色，默认'rgba(150,150,150,0.3)'
+			shadowWidth:'', // shadow的宽度，默认'auto'
+			shadowStyle:'' // shadow的形态('solid','dashed','double','auto')，默认'default'
+		}
+	},
+	toolbox:{ // 工具箱
+		show: true, // 是否展示，默认false
+		mark: true, // 辅助线标志，默认false
+		dataZoom: true, // 框选区域缩放，默认false
+		dataView: true, // 数据视图，默认false
+		magicType: true, // 动态类型切换，默认false
+		restore: true, // 还原，默认false
+		saveAsImage: true, // 保存图片，默认false
+	},
 	optionData:{
 		xAxis:[], // 横坐标数据
 	}
@@ -34,6 +60,8 @@
 ### 5. 参数
 > * 图表ID:*id*-->任意类型;必传
 > * 图例设置:*legend*-->Object;非必传，不传时或legend.data无数据不展示
+> * 数据提示框设置:*tooltip*-->Object;非必传
+> * 工具箱设置:*toolbox*-->Object;非必传
 > * 不同类型图标对应数据:*optionData*-->Object;必传
 
 ### 6. 样式
--- src/lib/vueChartsBlock.vue
+++ src/lib/vueChartsBlock.vue
@@ -4,7 +4,7 @@
 */
 /*
 * @LastEditors: aFei
-* @LastEditTime: 2018-11-29 15:48:25
+* @LastEditTime: 2018-12-03 11:49:16
 */
 <template>
   <div :id="id" class="vueChartsBlock extra"></div>
@@ -16,7 +16,7 @@
   require('echarts/lib/chart/line');
   require("echarts/lib/chart/pie");
   require('echarts/lib/component/tooltip');
-  require('echarts/lib/component/tooltip');
+  require('echarts/lib/component/toolbox');
   require('echarts/lib/component/legend');
 
   export default {
@@ -26,7 +26,22 @@
         required: true
       },
       legend: {
-        type: Object
+        type: Object,
+        default: () => {
+          return {show: false};
+        }
+      },
+      tooltip: {
+        type: Object,
+        default: () => {
+          return {show: false};
+        }
+      },
+      toolbox: {
+        type: Object,
+        default: () => {
+          return {show: false};
+        }
       },
       optionData: {
         type: Object,
@@ -49,6 +64,7 @@
       let vue = this; // 定义闭包变量
       this.charts = echarts.init(document.getElementById(this.id));
       this.charts.setOption({
+        // 图例
         legend: {
           show: this.legend.data === undefined || this.legend.data.length === 0 ? false : true,
           data: this.legend.data,
@@ -58,6 +74,52 @@
           textStyle: {
             color: this.legend.color === undefined ? '#000' : this.legend.color,
             fontSize: this.legend.fontSize === undefined ? 12 : this.legend.fontSize
+          }
+        },
+        // 数据提示框
+        tooltip: {
+          show: this.tooltip.show === undefined ? false : this.tooltip.show,
+          trigger: this.tooltip.trigger === undefined ? 'item' : this.tooltip.trigger,
+          formatter: this.tooltip.formatter === undefined ? null : this.tooltip.formatter,
+          axisPointer: {
+            type: this.tooltip.axisPointer === undefined ? 'line' : this.tooltip.axisPointer.type === undefined ? 'line' : this.tooltip.axisPointer.type,
+            lineStyle: {
+              color: this.tooltip.axisPointer === undefined ? '#48b' : this.tooltip.axisPointer.lineColor === undefined ? '#48b' : this.tooltip.axisPointer.lineColor,
+              width: this.tooltip.axisPointer === undefined ? 2 : this.tooltip.axisPointer.lineWidth === undefined ? 2 : this.tooltip.axisPointer.lineWidth,
+              type: this.tooltip.axisPointer === undefined ? 'solid' : this.tooltip.axisPointer.lineStyle === undefined ? 'solid' : this.tooltip.axisPointer.lineStyle
+            },
+            crossStyle: {
+              color: this.tooltip.axisPointer === undefined ? '#1e90ff' : this.tooltip.axisPointer.crossColor === undefined ? '#1e90ff' : this.tooltip.axisPointer.crossColor,
+              width: this.tooltip.axisPointer === undefined ? 1 : this.tooltip.axisPointer.crossWidth === undefined ? 1 : this.tooltip.axisPointer.crossWidth,
+              type: this.tooltip.axisPointer === undefined ? 'dashed' : this.tooltip.axisPointer.crossStyle === undefined ? 'dashed' : this.tooltip.axisPointer.crossStyle
+            },
+            shadowStyle: {
+              color: this.tooltip.axisPointer === undefined ? 'rgba(150,150,150,0.3)' : this.tooltip.axisPointer.shadowColor === undefined ? 'rgba(150,150,150,0.3)' : this.tooltip.axisPointer.shadowColor,
+              width: this.tooltip.axisPointer === undefined ? 'auto' : this.tooltip.axisPointer.shadowWidth === undefined ? 'auto' : this.tooltip.axisPointer.shadowWidth,
+              type: this.tooltip.axisPointer === undefined ? 'default' : this.tooltip.axisPointer.shadowStyle === undefined ? 'default' : this.tooltip.axisPointer.shadowStyle
+            }
+          }
+        },
+        // 工具箱
+        toolbox: {
+          show: this.toolbox.show === undefined ? false : this.toolbox.show,
+          mark: {
+            show: this.toolbox.mark === undefined ? false : this.toolbox.mark
+          },
+          dataZoom: {
+            show: this.toolbox.dataZoom === undefined ? false : this.toolbox.dataZoom
+          },
+          dataView: {
+            show: this.toolbox.dataView === undefined ? false : this.toolbox.dataView
+          },
+          magicType: {
+            show: this.toolbox.magicType === undefined ? false : this.toolbox.magicType
+          },
+          restore: {
+            show: this.toolbox.restore === undefined ? false : this.toolbox.restore
+          },
+          saveAsImage: {
+            show: this.toolbox.saveAsImage === undefined ? false : this.toolbox.saveAsImage
           }
         },
         xAxis: {
